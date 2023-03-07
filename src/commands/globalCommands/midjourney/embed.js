b74a0c005be64e9e43782762c6eafb6466a20960
jack444410 <jiekeisme@gmail.com>
2023-03-07 21:51:21 +0800

    Update embed.js

--- src/commands/globalCommands/midjourney/embed.js
+++ src/commands/globalCommands/midjourney/embed.js
@@ -15,7 +15,7 @@ async function responseFail(time ,user){
     return new EmbedBuilder()
     .setColor(0x0099FF)
     .setAuthor({ name: `${user.username+'#'+user.discriminator} 查詢了`, iconURL: `${user.avatarURL()}` })
-    .setDescription(`我們無法從您所指定的地區中查詢\n到相關資料，請確認是否輸入正確。`)
+    .setDescription(`產生失敗。`)
     .setFooter({ text: `jack.huang_14 技術開發 • ${time}`, iconURL: 'https://instagram.ftpe8-3.fna.fbcdn.net/v/t51.2885-19/328435729_146505831646207_4024874995475887638_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.ftpe8-3.fna.fbcdn.net&_nc_cat=107&_nc_ohc=L4ACOWXJvp4AX9V5jPi&edm=ACWDqb8BAAAA&ccb=7-5&oh=00_AfAMVrZwGkFql8Hx3Tbo9aGOAo3uFgv-ijZBgqS75oUY3Q&oe=640BE2A9&_nc_sid=1527a3' });
 
 }
